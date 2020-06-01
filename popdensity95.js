fe9e5d4ea41baadbdba13dded251d3c6dfb2f8a5
Raphael <raphael_zaafrani@hotmail.fr>
2020-06-01 02:33:44 -0700

    Fixes and new data set
    The first dataset I had scrapped was missing values so I scrapped a full one and formatted it to fit the topojson file
    I also fixed the map position and scale so now it should look good
    I just have to find a good scale for the colors because it goes from as low as 8 to 9000 so I'll do that in the morning

--- popdensity95.js
+++ popdensity95.js
@@ -6,30 +6,36 @@ Contructs a density map of communes from Val D'oise, France using D3
 /*eslint-env browser*/
 /*eslint no-console: 0*/
 /*global d3 */
+/*global topojson */
 
 //Credit to Mike Bostock for his California Population Density Code: https://bl.ocks.org/mbostock/5562380
 //Credit to Mike Adam Janes for his Choropleth V5 Code: https://bl.ocks.org/adamjanes/6cf85a4fd79e122695ebde7d41fe327f
 //Credit to Gregoire David for the geojson file: https://github.com/gregoiredavid/france-geojson
-//Credit to bien-dans-ma-ville.fr for the desity per commune: https://www.bien-dans-ma-ville.fr/classement-ville-densite-val-doise/
+//Credit to Wikipedia and the INSEE for the desity per commune: https://fr.wikipedia.org/wiki/Liste_des_communes_du_Val-d%27Oise https://www.insee.fr/fr/statistiques/2011101?geo=DEP-95
 //thanks to mygeodata for the conversion from geojson to topojson: https://mygeodata.cloud/
 
 
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+var width = 1100,
+    height = 960,
+    svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 var popDensity = d3.map();
 
-
-var path = d3.geoPath();
+var projection = d3.geoAlbers()
+    .scale( 90000 )
+    .rotate( [0,0] )
+    .center( [0, 42.313] )
+    .translate( [-width*1.5,height*11.5] );
+var path = d3.geoPath().projection(projection);
 
 var x = d3.scaleLinear()
     .domain([1, 10])
     .rangeRound([600, 860]);
 
-var color = d3.scaleThreshold()
-    .domain(d3.range(2, 10))
-    .range(d3.schemeBlues[9]);
+var color = d3.scaleQuantize([1, 1000], d3.schemeBlues[9])
 
 var g = svg.append("g")
     .attr("class", "key")
@@ -66,7 +72,7 @@ g.call(d3.axisBottom(x)
 
 var promises = [
   d3.json("communes-95.topojson"),
-  d3.tsv("stats.tsv", function(d) { popDensity.set(d.id, +d.rate); })
+  d3.csv("stats2.csv", function(d) { popDensity.set(d.nom, +d.val); })
 ]
 
 Promise.all(promises).then(ready)
@@ -78,10 +84,11 @@ function ready([vd]) {
     .selectAll("path")
     .data(communes.features)
     .enter().append("path")
-      .attr("fill", function(d) { return color(d.rate = popDensity.get(d.id)); })
+      .attr("stroke", 'black')
+      .attr("fill", function(d) {return color(d.val = popDensity.get(d.properties.nom)); })
       .attr("d", path)
     .append("title")
-      .text(function(d) { return d.rate + "%"; });
+      .text(function(d) {return d.properties.nom; });
 
   svg.append("path")
       .datum(topojson.mesh(vd, vd.objects.communes95.geometries, function(a, b) { return a !== b; }))
